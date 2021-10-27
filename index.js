d19dda267ae6e7194e824a35bce753644e7243e6
Veselin <vesselin60@gmail.com>
2021-10-27 17:11:34 +0300

    Get Dogecoin data

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-/**
- * Challenge: get a random image from Unsplash and set it as the background
- * 
- * Part 1:
- * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- * 
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
- */
-
 const author = document.querySelector('.author');
 
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg')
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -28,4 +10,11 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     })
     .catch(err => {
         document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1541788464282-8219dee793cc?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzUzMzk4NDQ&ixlib=rb-1.2.1&q=85')`;
+        author.textContent = `By: ${data.user.name}`;
     });
+
+fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => console.error(err));    
+
